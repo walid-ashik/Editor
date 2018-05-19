ca9569a30f4ff2fc3e4cd3e72f54041a395e9ba1
walid-ashik <walid.al.ashik@gmail.com>
2018-05-20 01:36:53 +0600

    function richEditor perfectly

---
+++ app/src/main/java/com/latenightcode/write/writer/MainActivity.java
@@ -0,0 +1,149 @@
+package com.latenightcode.write.writer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+import jp.wasabeef.richeditor.RichEditor;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RichEditor editor;
+    String doc;
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editor = findViewById(R.id.editor);
+
+        customizeRichEditor(editor);
+
+
+        findViewById(R.id.show).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doc = editor.getHtml();
+
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                i.putExtra("text", doc);
+                startActivity(i);
+
+            }
+        });
+
+
+    }
+
+    private void customizeRichEditor(final RichEditor mEditor) {
+
+        final RichEditor richEditor = mEditor;
+
+        richEditor.setEditorHeight(200);
+        richEditor.setEditorFontSize(18);
+        richEditor.setPadding(10, 10, 10, 10);
+        richEditor.setPlaceholder("Make this day a history...");
+        richEditor.setOnTextChangeListener(new RichEditor.OnTextChangeListener() {
+            @Override
+            public void onTextChange(String text) {
+
+            }
+        });
+
+        findViewById(R.id.action_undo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.undo();
+            }
+        });
+
+        findViewById(R.id.action_redo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.redo();
+            }
+        });
+
+        findViewById(R.id.action_bold).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setBold();
+            }
+        });
+        findViewById(R.id.action_italic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setItalic();
+            }
+        });
+        findViewById(R.id.action_blockquote).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setBlockquote();
+            }
+        });
+        findViewById(R.id.action_underline).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setUnderline();
+            }
+        });
+        findViewById(R.id.action_align_left).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setAlignLeft();
+            }
+        });
+        findViewById(R.id.action_align_center).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setAlignCenter();
+            }
+        });
+
+        findViewById(R.id.action_insert_bullets).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setBullets();
+            }
+        });
+
+        findViewById(R.id.action_insert_numbers).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setNumbers();
+            }
+        });
+        findViewById(R.id.action_heading2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setHeading(2);
+            }
+        });
+
+        findViewById(R.id.action_heading3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setHeading(3);
+            }
+        });
+
+        findViewById(R.id.action_heading4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                richEditor.setHeading(4);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+    }
+}
